36e5bbd03a4c69c9b12608ca2e21f9c4521b37de
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-08-27 08:22:56 -0300

    Listando agenda do prestador

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,8 +1,53 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
 
 class ScheduleController {
-  async index(req,res) {
-    return res.json();
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      res.status(400).json({ error: 'User are not provider.' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appoiments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appoiments);
   }
 }
 
