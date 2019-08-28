b14906b55024e7fa3f43714a7b4b5e9dbd20d882
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-08-28 02:07:28 -0300

    Cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -22,9 +22,11 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notification', NotificationController.index);
+routes.put('/notification/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
