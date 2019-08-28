6c6bbcec449357f299bd45119f0dcef91e80cdd1
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-08-28 10:54:19 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '86ca103604a00e',
+    pass: 'daaed854a7a19b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
