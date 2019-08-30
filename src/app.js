6d5bbd05bd90231d476cbb2f947482a198ab86f9
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-08-29 22:48:11 -0300

    tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'Youch';
 import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
 import sentryConfig from './config/sentry';
-import 'express-async-errors';
 
 class App {
   constructor() {
@@ -13,6 +14,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -27,6 +29,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    })
   }
 }
 
