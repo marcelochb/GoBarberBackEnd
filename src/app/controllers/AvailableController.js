298609a1b2ba4ce2f797cd2b3c4cf015cc68f941
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-30 20:17:53 -0300

    OK

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -7,21 +7,18 @@ import {
   format,
   isAfter,
 } from 'date-fns';
-import pt from 'date-fns/locale/pt';
-import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
 class AvailableController {
   async index(req, res) {
-    const { date, timezone } = req.query;
+    const { date } = req.query;
 
     if (!date) {
       return res.status(400).json({ error: 'Invalid date' });
     }
 
     const searchDate = Number(date);
-    const timezoneLocal = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     const appointments = await Appointment.findAll({
       where: {
@@ -60,48 +57,8 @@ class AvailableController {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
         available:
-          isAfter(
-            value,
-            format(
-              utcToZonedTime(new Date(), timezone),
-              'yyyy-MM-dd HH:mm:ssxxx',
-              {
-                timeZone: timezone,
-              }
-            )
-          ) && !appointments.find(a => format(a.date, 'HH:mm') === time),
-        newdata: new Date(),
-        UtcToZonedata: utcToZonedTime(new Date(), {
-          timeZone: timezone,
-          locale: pt,
-        }),
-        zonedTimeToUtc: zonedTimeToUtc(new Date(), {
-          timeZone: timezone,
-          locale: pt,
-        }),
-        formatData: format(
-          utcToZonedTime(new Date(), timezone),
-          'yyyy-MM-dd HH:mm:ssXXX',
-          {
-            timeZone: timezone,
-          }
-        ),
-        UtcToZoneformatData: utcToZonedTime(
-          format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx"),
-          {
-            timeZone: timezone,
-            locale: pt,
-          }
-        ),
-        ZoneToUtcformatData: zonedTimeToUtc(
-          format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx"),
-          {
-            timeZone: timezone,
-            locale: pt,
-          }
-        ),
-        timezone,
-        timezoneLocal,
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
       };
     });
 
