b5cd8365e482a0eae29d747b126abda15c4ef490
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-30 09:23:35 -0300

    fix: Date timezone on availablecontroller

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -7,18 +7,20 @@ import {
   format,
   isAfter,
 } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
 class AvailableController {
   async index(req, res) {
     const { date } = req.query;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     if (!date) {
       return res.status(400).json({ error: 'Invalid date' });
     }
 
-    const searchDate = Number(date);
+    const searchDate = utcToZonedTime(Number(date), { timeZone: timezone });
 
     const appointments = await Appointment.findAll({
       where: {
