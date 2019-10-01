6cc45b212acefb319cb5dbcad27eaf04c5008fea
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-10-01 07:51:24 -0300

    Utilizando Rate Limit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -28,6 +31,19 @@ class App {
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+
+    this.server.use(
+      new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+          }),
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 100,
+      })
     );
   }
 
